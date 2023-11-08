635fb3a871fdf58b493e2299218b004e73c767a6
kaspermads <kaspergm@hotmail.no>
2023-11-08 23:16:53 +0100

    Update navbar

--- nextjs-blog/components/navbar.js
+++ nextjs-blog/components/navbar.js
@@ -1,13 +1,12 @@
 import Head from 'next/head';
 
-import Router from 'next/router';
 import { AuthContext } from './AuthContext';
 import { useContext } from 'react';
 import Link from 'next/link';
 
 
 
-export default function Layout({ children }) {
+export default function Layout({ children, showNavbar = true }) {
   const { isAuthenticated, logout } = useContext(AuthContext);
   
   const handleLogout = async (e) => {
@@ -22,36 +21,39 @@ export default function Layout({ children }) {
       <Head>
         <title>Patient Portal</title>
       </Head>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Patient Portal</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="nav justify-content-center nav-underline ">
-              <li className="nav-item">
-                <Link href = "/home/patients-test">
-                  <a className="nav-link active" aria-current="page" href="#">Patients</a>
-                </Link>
-            </li>
-              <li className="nav-item">
-                <Link href = "/home/add-patient">
-                  <a className="nav-link" href="#">Add Patient</a>
-                </Link>
-            </li>
+      {showNavbar && (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Patient Portal</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="nav justify-content-center nav-underline ">
+                <li className="nav-item">
+                  <Link href="/home/patients-test">
+                    <a className="nav-link active" aria-current="page" href="#">Patients</a>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link href="/home/add-patient">
+                    <a className="nav-link" href="#">Add Patient</a>
+                  </Link>
+                </li>
            
              
-              {isAuthenticated && (
-                <li className="nav-item">
-                  <a className="nav-link" href="#" onClick={handleLogout} >Logout</a>
-                </li>
-              )}
-          </ul>
-        </div>
-        </div>
-      </nav>
+                {isAuthenticated && (
+                  <li className="nav-item">
+                    <a className="nav-link" href="#" onClick={handleLogout} >Logout</a>
+                  </li>
+                )}
+              </ul>
+            </div>
+          </div>
+        </nav>
+      )}
       <main>{children}</main>
+      
       
 
       </>
--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -2,13 +2,16 @@ import { AuthProvider }  from '../components/AuthContext';
 import '../styles/global.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import  Layout  from '../components/navbar';
-//hei hallo had
+import { useRouter } from 'next/router';
 
 
 export default function App({ Component, pageProps }) {
+
+  const router = useRouter();
+  const showNavbar = router.pathname !== '/home/login' && router.pathname !== '/home/register' && router.pathname !== '/';
   return (
     <AuthProvider>
-      <Layout>
+      <Layout  showNavbar={showNavbar}>
         <Component {...pageProps} />;
       </Layout>
     </AuthProvider>
