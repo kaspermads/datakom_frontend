22b1a4c0e15453a6007e8c74f1857a915fe69321
kaspermads <kaspergm@hotmail.no>
2023-11-08 23:06:42 +0100

    Update navbar

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -1,7 +1,7 @@
 import { AuthProvider }  from '../components/AuthContext';
 import '../styles/global.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Layout } from '../components/navbar';
+import  Layout  from '../components/navbar';
 //hei hallo had
 
 
