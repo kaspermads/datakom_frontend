14be67ce447e5325db576bf2319fadef341d7444
kaspermads <kaspergm@hotmail.no>
2023-12-13 01:09:48 +0100

    update styles

--- nextjs-blog/pages/home/patient/[patientId].js
+++ nextjs-blog/pages/home/patient/[patientId].js
@@ -116,7 +116,7 @@ const PatientDetail = () => {
       <div className="dashboardContainers">
         <h3>Oxygen Saturation data for {patient.first_name}</h3>
         {patient.patient_blood_oxygen_saturation_data && patient.patient_blood_oxygen_saturation_data.length > 0 ? (
-          <table className="table table-dark table-bordered table-striped table-sm w-50 p-3">
+          <table className="table custom-bp-table table-dark table-bordered table-striped table-sm w-50 p-3">
             <thead className = "thead-dark">
               <tr>
                 <th>Date</th>
