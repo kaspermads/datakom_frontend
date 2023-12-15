8005b3906882fe034fac8fdcfdd1847abe34dd2b
kaspermads <kaspergm@hotmail.no>
2023-12-15 12:25:22 +0100

    Add password help block

--- nextjs-blog/pages/home/register-test.js
+++ nextjs-blog/pages/home/register-test.js
@@ -80,7 +80,7 @@ const Register = () => {
                         required
                     />
                     <small id='passwordHelpBlock' className='form-text'>
-                        Your password must be 8-20 characters long, contain letters and numbers, and at Dleast one uppercase character.
+                        Your password must be 8-20 characters long, contain letters and numbers, and at least one uppercase character.
                     </small>
                 </div>
                 <div className="col-md-6">
